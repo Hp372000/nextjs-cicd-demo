43e576f476bb4d4a243e7a39b7299e2aba43cc19
Hp372000 <harshilhemalpatel@gmail.com>
2025-12-15 22:47:48 -0500

    updated index.js

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ export default function Home() {
       <h1>Hello World 🚀</h1>
       <p>Deployed using CI/CD to Azure VM</p>
       <p>This is just for demo</p>
+      <p> ci/cd works fine </p>
     </div>
   );
 }
